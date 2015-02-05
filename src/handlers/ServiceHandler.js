2b8b3765cb96814f72b8527793667e3f9a934e41
Joshua Kopman <joshuakopman@Joshuas-MacBook-Pro-2.local>
2015-02-04 20:41:25 -0500

    adding real API key

--- src/handlers/ServiceHandler.js
+++ src/handlers/ServiceHandler.js
@@ -4,7 +4,7 @@ function ServiceHandler(){
 }
 ServiceHandler.prototype.GetNeighborhoodByCoords = function(lat,lon,callback){
 
-        var url = 'https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&api_key=de0f0b6a92259a3f0dac0e1d2f2e4b91&lat='+lat+'&lon='+lon+'&format=json&nojsoncallback=1';
+        var url = 'https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&api_key=58c6594cbce90ae5daaa7ae687e1149f&lat='+lat+'&lon='+lon+'&format=json&nojsoncallback=1';
 
         https.get(url, function(res) {
             var body = '';
